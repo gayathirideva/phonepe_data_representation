ad7eec76939037a383b63fb26d37d14fb9c80452
gayathirideva <gayathirideva@users.noreply.github.com>
2023-10-31 12:32:27 +0530

    Add files via upload

---
+++ data_inter.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import mysql.connector as sql
+import streamlit as st
+import plotly.express as px
+import os
+import json
+from PIL import Image
+
+# Dataframe of aggregated Transactions
+#path1 = "C:/Users/akash/Phonepe_pulse/data/aggregated/transaction/country/india/state/"
+path1 ="C:/Users/l/repo_phonepe/pulse/data/aggregated/transaction/country/india/state"
+agg_trans_list = os.listdir(path1)
+
+columns1 = {'State': [], 'Year': [], 'Quarter': [], 'Transaction_type': [], 'Transaction_count': [],
+            'Transaction_amount': []}
+for state in agg_trans_list:
+    cur_state = path1 + state + "/"
+    agg_year_list = os.listdir(cur_state)
+    
+    for year in agg_year_list:
+        cur_year = cur_state + year + "/"
+        agg_file_list = os.listdir(cur_year)
+        
+        for file in agg_file_list:
+            cur_file = cur_year + file
+            data = open(cur_file, 'r')
+            A = json.load(data)
+            
+            for i in A['data']['transactionData']:
+                name = i['name']
+                count = i['paymentInstruments'][0]['count']
+                amount = i['paymentInstruments'][0]['amount']
+                columns1['Transaction_type'].append(name)
+                columns1['Transaction_count'].append(count)
+                columns1['Transaction_amount'].append(amount)
+                columns1['State'].append(state)
+                columns1['Year'].append(year)
+                columns1['Quarter'].append(int(file.strip('.json')))
+                
+#df_agg_trans = pd.DataFrame(columns1)
+#df_agg_trans.show()
